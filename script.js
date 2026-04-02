982083e3dfab3b553caa2174912622a8419d9d45
Karthikg08 <kart03309@gmail.com>
2026-04-02 14:53:46 +0530

    resume button added in the header , social icon in home page are make it interactive

--- script.js
+++ script.js
@@ -39,5 +39,15 @@
         }
 
 
+        function showsidebar(){
+            const sidebar = document.querySelector('.sidebar');
+            sidebar.style.display = 'flex';
+        }
+
+        function closesidebar(){
+            const sidebar = document.querySelector('.sidebar');
+            sidebar.style.display = 'none';
+        } 
+
 
       
