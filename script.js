faf377db2fd0a72ea1e8993754d11c3196ad329d
Karthikg08 <kart03309@gmail.com>
2026-04-08 17:46:50 +0530

    made responsive design

--- script.js
+++ script.js
@@ -39,15 +39,9 @@
         }
 
 
-        function showsidebar(){
-            const sidebar = document.querySelector('.sidebar');
-            sidebar.style.display = 'flex';
-        }
 
-        function closesidebar(){
-            const sidebar = document.querySelector('.sidebar');
-            sidebar.style.display = 'none';
-        } 
+
+
 
 
       
