c9d338ff6f58593f426f351277bbb3e163841090
Karthikg08 <kart03309@gmail.com>
2026-04-10 20:22:46 +0530

    try to do add animations in the webpage

--- script.js
+++ script.js
@@ -69,3 +69,21 @@ function showMessage(text, type) {
         formMessage.classList.remove('success', 'error', 'info');
     }, 5000);
 }
+
+// scroll animations //
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            console.log(entry.target)
+            entry.target.classList.add('visible');
+        } else {
+            entry.target.classList.remove('visible');
+        }
+    })
+}, {threshold: 0.1  })
+
+
+document.querySelectorAll('.home, .about, .skills, .projects, .contact').forEach((section) => {
+    observer.observe(section);
+})
